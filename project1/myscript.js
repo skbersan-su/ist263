662150130ebdf6f41644444e98bc9c59bcb0278a
skbersan-su <skbersan@syr.edu>
2020-11-23 20:19:29 -0500

    update

--- project1/myscript.js
+++
@@ -1,9 +0,0 @@
-
-function TurnNavOnOff() {
-  if (document.getElementById("navlinks").style.getPropertyValue("display") =='block') {
-       document.getElementById('navlinks').style.display = 'none';
-        }
-    else {
-     document.getElementById('navlinks').style.display = 'block';
-  }
- }
