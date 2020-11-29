da647b8cc56a87011a3b19918d91f4e3709dead7
skbersan-su <skbersan@syr.edu>
2020-11-28 20:57:30 -0500

    update

---
+++ project1/myscript.js
@@ -0,0 +1,9 @@
+
+function TurnNavOnOff() {
+  if (document.getElementById("navlinks").style.getPropertyValue("display") =='block') {
+       document.getElementById('navlinks').style.display = 'none';
+        }
+    else {
+     document.getElementById('navlinks').style.display = 'block';
+  }
+ }
